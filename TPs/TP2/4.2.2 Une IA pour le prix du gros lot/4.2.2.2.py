f396ebb686700db7e184865c863558f0cddfd600
Anastasiia <anastasia.milenkova.18@gmail.com>
2024-09-24 19:24:35 +0200

    Added .gitignore file

--- TPs/TP2/4.2.2 Une IA pour le prix du gros lot/4.2.2.2.py
+++ TPs/TP2/4.2.2 Une IA pour le prix du gros lot/4.2.2.2.py
@@ -1,3 +1,16 @@
+
+
+
+"""
+-----------------------------------------------------------------------------
+i11_Milenkova_BassChervony_4.2.2.2.py : CR « Une IA por le prix du gros lot », groupe IMA_o5_P
+
+Anastasiia Milenkova <Anastasiia.Milenkova@etu.univ-grenoble-alpes.fr>
+Daniel Bass Chervony <Daniel.Bass@etu.univ-grenoble-alpes.fr>
+-----------------------------------------------------------------------------
+""" 
+
+
 from random import randint      # on importe de module random le methode randint 
 
 # l'initialisation des constantes
@@ -34,3 +47,59 @@ while rep!="b":
 # quand on sort de la boucle rep="b", le programme a devine le nombre choisi par l'util
 print(MSG_GAGNE)
 print(MSG_ESS,n_ess)
+
+
+"""
+TEST 1:
+Je cherche un nombre entre 0 et 100
+Je propose  26  ? p
+Je cherche un nombre entre 27 et 100
+Je propose  91  ? g
+Je cherche un nombre entre 27 et 90
+Je propose  90  ? g
+Je cherche un nombre entre 27 et 89
+Je propose  66  ? p
+Je cherche un nombre entre 67 et 89
+Je propose  83  ? g
+Je cherche un nombre entre 67 et 82
+Je propose  81  ? g
+Je cherche un nombre entre 67 et 80
+Je propose  68  ? p
+Je cherche un nombre entre 69 et 80
+Je propose  77  ? g
+Je cherche un nombre entre 69 et 76
+Je propose  70  ? p
+Je cherche un nombre entre 71 et 76
+Je propose  75  ? g
+Je cherche un nombre entre 71 et 74
+Je propose  74  ? g
+Je cherche un nombre entre 71 et 73
+Je propose  73  ? b
+J’ai gagné
+Le nombre d'essais est : 12
+oskorusha@anna-osk-pc:~/Documents/uni/INF101$ ^C
+oskorusha@anna-osk-pc:~/Documents/uni/INF101$ ^C
+
+ cd /home/oskorusha/Documents/uni/INF101 ; /usr/bioskorusha@anna-osk-pc:~/Documents/uni/INF101$  cd /home/oskorusha/Documents/uni/INF101 ; /usr/bin/env /bin/python3 /home/oskorusha/.vscode/extensions/ms-python.debugpy-2024.8.0-linux-x64/bundled/libs/debugpy/adapter/../../debugpy/launcher 57459 -- /home/oskorusha/Documents/uni/INF101/TPs/TP2/4.2.2\ Une\ IA\ pour\ le\ prix\ du\ gros\ lot/4.2.2.2.py 
+Je cherche un nombre entre 0 et 100
+Je propose  18  ? p
+Je cherche un nombre entre 19 et 100
+Je propose  33  ? p
+Je cherche un nombre entre 34 et 100
+Je propose  93  ? g
+Je cherche un nombre entre 34 et 92
+Je propose  74  ? g
+Je cherche un nombre entre 34 et 73
+Je propose  62  ? g
+Je cherche un nombre entre 34 et 61
+Je propose  45  ? p
+Je cherche un nombre entre 46 et 61
+Je propose  56  ? p
+Je cherche un nombre entre 57 et 61
+Je propose  58  ? p
+Je cherche un nombre entre 59 et 61
+Je propose  61  ? b
+J’ai gagné
+Le nombre d'essais est : 9
+
+"""
