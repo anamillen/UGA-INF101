23f64f10643d5c9b980282f0399bc1f6a7471f55
Anastasiia <anastasia.milenkova.18@gmail.com>
2024-10-14 15:43:35 +0200

    Exo 4.4.4 du TP4 est termine

--- TPs/TP4/4.4.4 Un peu d’arithmétique/4.4.4 Un peu d’arithmétique.py
+++ TPs/TP4/4.4.4 Un peu d’arithmétique/4.4.4 Un peu d’arithmétique.py
@@ -66,7 +66,129 @@ def plus_grand_diviseur_premier(n):
     return res
     
 
+def pgcd_naif(a,b):
+    """Renvoie le plus grand commun diviseur des entiers a et b.
+    Le calcule en utilisant l'algorithme naif
+    
+    Exemples :
+    >>> pgcd_naif(12, 8)
+    4
+    >>> pgcd_naif(17, 13)
+    1
+    >>> pgcd_naif(100, 25)
+    25
+    >>> pgcd_naif(48, 18)
+    6
+    >>> pgcd_naif(7, 1)
+    1
+    >>> pgcd_naif(0, 5)
+    5
+    >>> pgcd_naif(0, 0)
+    0
+    >>> pgcd_naif(3,0)
+    3
+    """
+
+    # comme on peut pas diviser par zero, on verifie d'abord si a ou b
+    # est egale a 0. si a = 0 ou b = 0 alors pcgd = nombre non-nul
+    if a == 0:
+        i = b 
+    elif b == 0:
+        i = a
+    else:    # si a,b!=0 alors on procede au calcul du pgcd
+        if a>b :  # on trouve le nombre plus grand pour diminuir le n de nos candidats
+            a,b = b,a
+        i = a
+        while not (a%i==0 and b%i==0):   # on parcourt chaque  nombre <min(a,b) jusqu'a on a trouve pgcd
+            i-=1
+    return i
+
+def pgcd(a,b):
+    """Renvoie le plus grand commun diviseur des entiers a et b.
+    Le calcule en utilisant l'algorithme d'Euclide
+    
+    Exemples :
+    >>> pgcd(12, 8)
+    4
+    >>> pgcd(17, 13)
+    1
+    >>> pgcd(100, 25)
+    25
+    >>> pgcd(48, 18)
+    6
+    >>> pgcd(7, 1)
+    1
+    >>> pgcd(0, 5)
+    5
+    >>> pgcd(0, 0)
+    0
+    """
+    # on implemente l'algorithme euclideen
+    while b != 0:
+        a, b = b, a % b
+    return a
+
+def ppcm(a,b):
+    """
+    Renvoie le plus petit commun multiple de a et b
+
+    Exemples :
+    >>> ppcm(24,12)
+    24
+    >>> ppcm(9,16)
+    144
+    >>> ppcm(7,3)
+    21
+    >>> ppcm(0,25)
+    0
+    >>> ppcm(35,5)
+    35
+    >>> ppcm(10,10)
+    10
+    """
+    # on sait que ppcm  = a*b/pgcd(a,b)
+    return int(a*b/pgcd(a,b))
+
+def irreductible(num,den):
+    """Calcule un booléen indiquant si la fraction 
+    num/dem est irréductible ou non
+    
+    Exemples :
+    >>> irreductible(5,3)
+    True
+    >>> irreductible(0,7)
+    False
+    >>> irreductible(14, 7)
+    False
+    >>> irreductible(5,10)
+    False
+    >>> irreductible(3,2)
+    True
+    >>> irreductible(2,1)
+    True
+    """
+    # On vérifie si le PGCD du numérateur et du dénominateur est 1
+    return pgcd(num, den) == 1
+
+
+def nombres_par_pgcd_et_ppcm(pgcd,ppcm):
+    """Reçoit 2 entiers pgcd et ppcm, et affiche tous les couples
+    d’entiers a et b dans l’ordre croissant dont ces nombres sont le pgcd et le ppcm
+
+    """
+    prod = pgcd*ppcm
+    a = pgcd
+    while a<=prod%2:
+        if prod%a==0:
+            print(a, prod/a)
+        a+=1
+
+
+
 
 # programme principal
-doctest.testmod()
 
+cd = int(input())
+cm = int(input())
+
+nombres_par_pgcd_et_ppcm(cd,cm)
