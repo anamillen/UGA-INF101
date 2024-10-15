6d51d5c0bb76b64a72eee35b04ea044e94375806
Anastasiia <anastasia.milenkova.18@gmail.com>
2024-10-15 06:39:52 +0200

    TP4 suite de Fibonacci est finie

--- TPs/TP4/4.4.5 Suite de Fibonacci/4.4.5 Suite de Fibonacci.py
+++ TPs/TP4/4.4.5 Suite de Fibonacci/4.4.5 Suite de Fibonacci.py
@@ -1,9 +1,8 @@
 # on importe les modules necessaires
 import doctest
-import scipy.constants
+from scipy import constants
 
 # definitions de fonctions
-
 def fibonacci(N):
     """Calcule n-ieme nombre de Fibonacci
     
@@ -69,7 +68,8 @@ def liste_de_termes(n):
     return l
 
 def nombre_d_or(N):
-    """Renvoie la n-ième estimation du nombre d’or fn/f(n-1)
+    """Renvoie la n-ième estimation du nombre d’or fn/f(n-1).
+    Ou n>=2
     
     Exemples :
     >>> nombre_d_or(2)
@@ -90,23 +90,31 @@ def nombre_d_or(N):
     # a partir d'ici i = N
     return float(f1/f0)
 
-"""
+
 def precision_ndor(eps):
+    """Calcule le nombre minimal de termes (n) nécessaires pour obtenir une approximation du nombre d'or
+    avec une précision donnée (eps). Le nombre d'or est approximé par la relation suivante :
+    nombre_d_or(n) = F(n+1) / F(n), où F(n) est le n-ième terme de la suite de Fibonacci.
+
+    Exemples :
+    >>> precision_ndor(1e-2)
+    7
+    >>> precision_ndor(1e-5)
+    14
+    >>> precision_ndor(1e-10)
+    26
+    """
     i = 2
-    NDOR = scipy.constants.golden
-    ndor_cur = nombre_d_or
-    while NDOR-eps!=ndor_cur or NDOR+eps!=ndor_cur:
+    NDOR = constants.golden
+    ndor_cur = nombre_d_or(i)
 
-"""
-
-
-
-
-
-
+    # tant que la distance entre le nombre d'or actuel et ndor calcule est > eps on continue a boucler
+    while abs(NDOR-ndor_cur)>eps:
+        i+=1
+        ndor_cur = nombre_d_or(i)
+    return i
 
 
 
 # programme principal
-
 doctest.testmod()
