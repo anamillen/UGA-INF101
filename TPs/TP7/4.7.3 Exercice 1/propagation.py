af2026163b4c614462da109ba1f6e06a32903425
Anastasiia <anastasia.milenkova.18@gmail.com>
2024-11-17 08:11:03 +0100

    TP7 exercice 7 corrige

--- TPs/TP7/4.7.3 Exercice 1/propagation.py
+++ TPs/TP7/4.7.3 Exercice 1/propagation.py
@@ -1,121 +1,80 @@
-# on importe les modules necessaires
 import random
-
-# definitions de fonctions 
-def initialisation(N):
-    """Initialise l'etat initial"""
-    return ["C"]+["I"]*(N-1)
-
-def choix_personne(N):
-    """Choisit l'indice d'une personne pour la rencontre et le renvoie"""
-    return random.randint(0,N-1)
-
-def tirage(N):
-    """Renvoie les indices de 2 personnes choisies aletoirement pour une rencontre"""
-    i1 = choix_personne(N)
-    i2 = choix_personne(N)
-    while i1==i2:
-        i2 = choix_personne(N)
-    return i1,i2
-
-def rencontre(p1,p2):
-    """Simulation d'une rencontre entre p1 et p2.
-    Renvoie les valeurs obtenues par p1 et p2 apres la rencontre"""
-    pp = [p1,p2]
-    if c_present(pp):
-        if p1==p2:
-            p1,p2 = "M","M"
-        else:  # si p1 et p2 n'ont pas le meme statut
-            if "M" in pp:
-                p1,p2 = "M","M"
-            else:
-                p1,p2 = "C","C"
-    return p1, p2
-
-def change_indices(N, l1, l2):
-    """Renvoie les indices de ces elements qui ont change stockes dans une liste"""
-    res = []
-    for i in range(N):
-        if l1[i]!=l2[i]:
-            res.append(i)
-    return res
 
 def c_present(li):
     """Verifie s'il y en a des personnes connaissantes la nouvelle"""
     return "C" in li
 
-def modification(n1, n2, per_avant):
-    """Renvoie une liste deja modifiee apres la rencontre"""
-    per_apres = list(per_avant)
-    per_apres[n1], per_apres[n2] = rencontre(per_avant[n1], per_avant[n2])
-    return per_apres
+def initialisation(N):
+    """Initialise l'état initial avec la personne 0 connaissant la nouvelle."""
+    return ["C"] + ["I"] * (N - 1)
 
-def jour(N, n1, n2, per_avant):
-    """Fait les changements de chaque jour"""
-    per_apres = modification(n1,n2,per_avant)
-    changement = change_indices(N, per_avant, per_apres)
-    return per_apres, changement
+def choix_personne(N):
+    """Renvoie l'indice d'une personne choisie au hasard."""
+    return random.randint(0, N - 1)
 
-def compter_ignor(personnes):
-    """Compte le nb de personnes ignorantes en personnes"""
-    return personnes.count("I")
+def tirage(N):
+    """Renvoie deux indices distincts de personnes choisies au hasard."""
+    i1 = choix_personne(N)
+    i2 = choix_personne(N)
+    while i1 == i2:     # Assure que les indices soient différents.
+        i2 = choix_personne(N)
+    return i1, i2
 
-def compter_muet(personnes):
-    """Compte le nb de personnes muettes en personnes"""
-    return personnes.count("M")
+def rencontre(p1, p2):
+    """Retourne les nouveaux statuts de deux personnes après une rencontre."""
+    pp = [p1,p2]
+    if c_present(pp):       # Si au moins une personne connaît la nouvelle
+        if p1==p2:          # Les deux personnes ont le même statut
+            p1,p2 = "M","M"
+        else:               # Statuts différents
+            if "M" in pp:   # Si l'une des personnes est muette
+                p1,p2 = "M","M"
+            else:           # Sinon l'autre personne apprend la nouvelle
+                p1,p2 = "C","C"
+    return p1, p2
 
+def modification_statuts(n1, n2, personnes):
+    """Applique une rencontre et retourne la nouvelle liste de statuts."""
+    personnes_modifiees = list(personnes)   # Copie de la liste initiale
+    p1, p2 = personnes[n1], personnes[n2]
+    personnes_modifiees[n1], personnes_modifiees[n2] = rencontre(p1, p2)
+    return personnes_modifiees
 
-# les fonctions avec les effets de bord
-def paire_choisie_affiche(n1,n2, personnes):
-    """Affiche les nombres de personnes choisises et leurs natures"""
-    print(str(n1)+",",n2,"\t\t", personnes[n1], personnes[n2], end = "\t")
-
-def changement_affiche(N,changement, personnes):
-    """Affiche les changements"""
+def afficher_entete(N):
+    """Affiche l'entête du tableau de simulation."""
+    print("jour".ljust(5), "rencontre".ljust(10), "nature".ljust(10), end="")
     for i in range(N):
-        if i in changement:
-            print(str(personnes[i]).rjust(0), end="     ")
-        else:
-            print("-".rjust(0), end="     ")
-
-def header(N):
-    """Affiche le 'chapeau' de l'affichage"""
-    print(
-        "jour",
-        "rencontre",
-        "nature",
-        sep="\t",
-        end="\t"
-    )
-    for i in range(N):
-        print("st."+str(i),end="  ")
+        print(("st."+str(i)).ljust(5), end="")
     print()
 
-def msg(personnes):
-    """Affiche les derniers messages qui comptent combien il y a de muetes et de ignorants"""
-    ignor = compter_ignor(personnes)
-    print("Nombre d'ignorants :",ignor)
-    print("Nombre de muets :",len(personnes)-ignor)
+def afficher_etat(jour, n1, n2, personnes_avant, personnes_apres):
+    """Affiche l'état de la simulation pour un jour donné."""
+    ind = str(n1)+", "+str(n2)
+    nature = personnes_avant[n1]+", "+personnes_avant[n2]
+    print(str(jour).ljust(6), ind.ljust(10), nature.ljust(10), end="")
+    for i in range(len(personnes_apres)):
+        if personnes_avant[i] != personnes_apres[i]:    # Affiche uniquement si le statut a changé
+            print(personnes_apres[i].ljust(5), end="")
+        else:
+            print("-".ljust(5), end="")     # Sinon, affiche un tiret
+    print()
 
 def simulation_en_detail(N):
-    # initialisation 
+    """Simule la propagation d'une nouvelle dans une population en affichant les étapes."""
     personnes = initialisation(N)
+    afficher_entete(N)
     jour_decompt = 1
-    while c_present(personnes):
-        print(str(jour_decompt).rjust(3), end="\t  ")
+    while c_present(personnes):     # Continue tant qu'il reste des personnes avec le statut 'C'
         n1, n2 = tirage(N)
-        paire_choisie_affiche(n1, n2, personnes)
-        personnes, changement = jour(N, n1,n2, personnes)
-        changement_affiche(N, changement, personnes)
-        jour_decompt+=1
-        print()
-    # a partir d'ici l n'y a pas de personnes "C" parmis toutes les personnes
-    msg(personnes)
+        personnes_apres = modification_statuts(n1, n2, personnes)
+        afficher_etat(jour_decompt, n1, n2, personnes, personnes_apres)
+        personnes = personnes_apres
+        jour_decompt += 1
+    
+    # Résumé final
+    print("\nNombre d’ignorants :", personnes.count("I"))
+    print("Nombre de muets :", personnes.count("M"))
 
-
-
-# programme principal pour tester
-N = 3
-header(N)
+# Programme principal
+N = 5  # Taille de la population
 simulation_en_detail(N)
-
